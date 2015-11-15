7cf766d138ea8aece25aa3d4563f0da0ea4e221c
NemoAir <wangjienemo@gmail.com>
2015-11-15 18:29:05 +0800

    创建工程

---
+++ NemoAlertViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NemoAlertViewDemo
+//
+//  Created by Nemo on 15/11/15.
+//  Copyright © 2015年 Nemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NemoAlertViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NemoAlertViewDemo
+//
+//  Created by Nemo on 15/11/15.
+//  Copyright © 2015年 Nemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
